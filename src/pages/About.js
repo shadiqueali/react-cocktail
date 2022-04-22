5c724ca7f8c2a90af0ff519941a85757196ab6b2
shadiqueali <sadikali322@gmail.com>
2022-04-22 14:43:32 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos blanditiis totam laborum et laboriosam,
+                natus voluptates molestias. Sit, tempore molestiae laudantium quos obcaecati aspernatur veniam nulla
+                suscipit reiciendis reprehenderit, dicta quas? Asperiores fugiat unde blanditiis eum numquam ad aliquam
+                possimus!
+            </p>
+        </section>
+    );
+};
+
+export default About;
